28d0d4e38c71728f2c989cf7ac55b11013c8b875
Doug Meyer <doug.meyer@vibes.com>
2019-01-03 21:23:26 -0600

    More working solitare

---
+++ solitare.js
@@ -0,0 +1,208 @@
+export const name = 'Solitare';
+
+const suits = 'Heart Diamond Spade Club'.split(' ');
+const cards = new Array(52).fill().map((_, index) => ({
+  id: index,
+  suit: suits[Math.floor(index / 13)],
+  value: index % 13 + 1
+}));
+
+/* API
+items: []
+x, y, width, height, image
+canPick, pick
+canPlace, place
+*/
+
+class Card {
+  static get url() { return `./Contemporary_playing_cards.svg`; }
+  static get width() { return 72; }
+  static get height() { return 110; }
+  static get padding() { return 10; }
+  static get peek() { return 25; }
+  static pos(x, y) {
+    return [
+      Card.padding + x * (Card.width + Card.padding),
+      Card.padding + y * (Card.height + Card.padding)
+    ];
+  }
+  constructor(game, id, suit, value) {
+    Object.assign(this, {
+      game, id, suit, value, revealed: false
+    });
+  }
+  get image() {
+    if (this.revealed) return `${Card.url}#${this.value}_${this.suit}`;
+    return `${Card.url}#Back`;
+  }
+  get stack() { return this.game.items.find(stack => stack.items.includes(this)); }
+  get x() { return this.stack.cardPos(this).x; }
+  get y() { return this.stack.cardPos(this).y; }
+  pick({ altKey, ctrlKey, metaKey, shiftKey }){
+    if (shiftKey) {
+      const goalStack = this.game.goalStacks.find(stack => stack.canPlace(this));
+      const cardLastOnStack = this === this.stack.items.slice(-1)[0];
+      if (goalStack && cardLastOnStack) goalStack.items.push(this.stack.items.splice(-1)[0]);
+      this.game.trigger();
+    } else {
+      this.stack.pick(this);
+    }
+  }
+  place(){ this.stack.place(this); }
+}
+class Stack {
+  constructor(game, x, y) {
+    Object.assign(this, {
+      game, x, y, items: []
+    });
+  }
+  get image() { return `${Card.url}#stack`; }
+  cardPos(card) {
+    const index = this.items.indexOf(card);
+    return { x: this.x, y: this.y + Math.min(index, 3) };
+  }
+}
+class DrawStack extends Stack {
+  constructor(game, x, y) {
+    super(game, x, y);
+    cards.slice().sort(() => Math.random() - 0.5).forEach(({ id, suit, value }) => {
+      this.items.push(new Card(game, id, suit, value))
+    })
+  }
+  pick(_card){
+    const { discardStack } = this.game;
+    if (this.items.length) {
+      const card = this.items.pop();
+      card.revealed = true;
+      discardStack.items.push(card);
+    } else {
+      while (discardStack.items.length) {
+        const card = discardStack.items.pop();
+        card.revealed = false;
+        this.items.push(card);
+      }
+    }
+    this.game.trigger();
+  }
+  place(_card){}
+}
+class DiscardStack extends Stack {
+  pick(_card){
+    this.game.holdingStack.grab(this.items.splice(-1), this);
+    this.game.trigger();
+  }
+  place(_card){}
+}
+class GoalStack extends Stack {
+  pick(_card){
+    this.game.holdingStack.grab(this.items.splice(-1), this);
+    this.game.trigger();
+  }
+  canPlace(card){
+    const lastOnStack = this.items.slice(-1)[0];
+    return lastOnStack
+      ? card.suit === lastOnStack.suit &&
+        card.value === lastOnStack.value + 1
+      : card.value === 1;
+  }
+  place(_card){
+    const canPlace = this.game.holdingStack.items.length === 1 && this.canPlace(this.game.holdingStack.items[0]);
+    if (!canPlace) return;
+    this.items.push(...this.game.holdingStack.items.splice(0));
+    this.game.trigger();
+  }
+}
+class CascadingStack extends Stack {
+  pick(card) {
+    const index = this.items.indexOf(card);
+    if (index === -1) return;
+    if (card.revealed) {
+      this.game.holdingStack.grab(this.items.splice(index), this);
+      this.game.trigger();
+    } else if (index === this.items.length - 1) {
+      this.items.slice(-1)[0].revealed = true;
+      this.game.trigger();
+    }
+  }
+  place(_card) {
+    const lastOnStack = this.items.slice(-1)[0];
+    const firstHealdCard = this.game.holdingStack.items[0];
+    if (!firstHealdCard) return;
+    const canPlace = this.game.holdingStack.items.length &&
+      lastOnStack
+      ? lastOnStack.value - 1 === firstHealdCard.value &&
+        Math.floor(suits.indexOf(lastOnStack.suit)/2) !== Math.floor(suits.indexOf(firstHealdCard.suit)/2)
+      : firstHealdCard.value === 13;
+    if (!canPlace) return;
+    this.items.push(...this.game.holdingStack.items.splice(0));
+    this.game.trigger();
+  }
+  cardPos(card) {
+    const index = this.items.indexOf(card);
+    const offset = this.items.slice(0, index).reduce((acc, { revealed }) => acc + (revealed ? Card.peek : Card.padding), 0);
+    return { x: this.x, y: this.y + offset };
+  }
+}
+class HoldingStack {
+  constructor(game) {
+    Object.assign(this, {
+      game, x: 0, y: 0, items: []
+    });
+  }
+  cardPos(card) {
+    const index = this.items.indexOf(card);
+    return { x: this.x, y: this.y + index * Card.peek };
+  }
+  grab(cards, stack) {
+    this.sourceStack = stack;
+    this.x = stack.cardPos(cards[0]).x;
+    this.y = stack.cardPos(cards[0]).y;
+    this.items.push(...cards);
+  }
+  reset() {
+    if (this.sourceStack) {
+      this.sourceStack.items.push(...this.items.splice(0));
+      this.game.trigger();
+    }
+    this.sourceStack = null;
+  }
+}
+
+export default class Solitare {
+  constructor() {
+    [ this.width, this.height ] = Card.pos(7, 4);
+    this.holdingStack = new HoldingStack(this);
+    this.drawStack = new DrawStack(this, ...Card.pos(0, 0));
+    this.discardStack = new DiscardStack(this, ...Card.pos(1, 0));
+    this.goalStacks = new Array(4).fill().map((_, i) => new GoalStack(this, ...Card.pos(3+i, 0)));
+    this.cascadingStacks = new Array(7).fill().map((_, i) => new CascadingStack(this, ...Card.pos(i, 1)));
+    this.items = [
+      this.drawStack,
+      this.discardStack,
+      ...this.goalStacks,
+      ...this.cascadingStacks,
+      this.holdingStack
+    ];
+    this.items.forEach((stack, index) => { stack.id = `stack_${index}`; });
+    this.subscriptions = [];
+    // deal
+    for (let i=0; i<7; i++) {
+      for (let j=i; j<7; j++) {
+        this.cascadingStacks[j].items.push(this.drawStack.items.pop());
+      }
+      this.cascadingStacks[i].items.slice(-1)[0].revealed = true;
+    }
+  }
+  subscribe(fn) {
+    this.subscriptions.push(fn);
+    return function unsubscribe(){
+      const index = this.subscriptions.indexOf(fn);
+      if (index === -1) return false;
+      this.subscriptions.splice(index, 1);
+      return true;
+    }.bind(this);
+  }
+  trigger() {
+    this.subscriptions.forEach(fn => fn());
+  }
+}
--- table_games.js
+++ table_games.js
@@ -1,232 +1,101 @@
 import { html, render, Component } from 'https://unpkg.com/htm/preact/standalone.mjs';
 
-const width = 72;
-const height = 110;
-class Card extends Component {
-  constructor(props) {
+import Solitare from './solitare.js';
+
+class Item extends Component {
+  constructor(props){
     super(props);
     this.handleMouseDown = this.handleMouseDown.bind(this);
-    this.handleReveal = this.handleReveal.bind(this);
+    this.handleMouseUp = this.handleMouseUp.bind(this);
   }
-
-  handleMouseDown(e){
-    this.props.onMouseDown(this.props.index, e);
+  handleMouseDown(event){
+    event.preventDefault();
+    const { altKey, ctrlKey, metaKey, shiftKey } = event;
+    const { item } = this.props;
+    item.pick({ altKey, ctrlKey, metaKey, shiftKey });
   }
-  handleReveal(e){
-    e.preventDefault();
-    this.props.onReveal(this.props.index);
+  handleMouseUp(){
+    const { altKey, ctrlKey, metaKey, shiftKey } = event;
+    const { item } = this.props;
+    item.place({ altKey, ctrlKey, metaKey, shiftKey });
   }
-
-  render({ revealed, value, suite, x, y, animated }) {
-    return html`<use
-      xlink:href=${`./Contemporary_playing_cards.svg#${revealed ? `${value}_${suite}` : 'Back'}`}
-      x=${x} y=${y}
-      class=${`interactable Card${animated ? ' animated' : ''}`}
+  render({ item, image, ...props }){
+    return html`<use ...${props}
+      xlinkHref=${image}
+      width=${item.width} height=${item.height}
+      class=${`Card ${item.stack === item.game.holdingStack ? '' : 'animated'}`}
       onMouseDown=${this.handleMouseDown}
-      onContextMenu=${this.handleReveal}
+      onMouseUp=${this.handleMouseUp}
     />`;
   }
 }
-
-const sleep = seconds => new Promise(function(resolve){ setTimeout(resolve, 1000 * seconds); });
 
 class TableGames extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { cards: [] };
+    this.state = { holding: { dx: 0, dy: 0 } };
+    this.dragStart = { x: 0, y: 0 };
 
     this.startSolitare();
 
-    this.handleReveal = this.handleReveal.bind(this);
     this.handleMouseDown = this.handleMouseDown.bind(this);
-    this.handleMouseUp = this.handleMouseUp.bind(this);
     this.handleMouseMove = this.handleMouseMove.bind(this);
-    this.startShuffle = this.startShuffle.bind(this);
+    this.handleMouseUp = this.handleMouseUp.bind(this);
     this.startSolitare = this.startSolitare.bind(this);
   }
 
-  async startShuffle(){
-    let { cards } = this.state;
-    this.setState({ stacks: [] });
-    for (let i=cards.length-1,card; card=cards[i]; i--){
-      card.animated = true;
-      card.x = `${Math.random() * 80}%`;
-      card.y = `${Math.random() * 80}%`;
-      this.setState({ cards });
-      await sleep(0.01);
-    }
-    for (let i=0,card; card=cards[i]; i++){
-      card.revealed = false;
-      this.setState({ cards });
-      await sleep(0.01);
-    }
-    cards = cards.sort(_ => Math.random() - 0.5);
-    for (let i=0,card; card=cards[i]; i++){
-      card.x = 10;
-      card.y = 10;
-      this.setState({ cards });
-      await sleep(0.01);
-    }
-    for (let i=0,card; card=cards[i]; i++){
-      card.animated = false;
-    }
-  }
   async startSolitare(){
-    const stacks = [
-      { x: 10 + (width+10)*0, y: 10 + (height+10)*0 },
-      { x: 10 + (width+10)*1, y: 10 + (height+10)*0 },
-
-      { x: 10 + (width+10)*3, y: 10 + (height+10)*0 },
-      { x: 10 + (width+10)*4, y: 10 + (height+10)*0 },
-      { x: 10 + (width+10)*5, y: 10 + (height+10)*0 },
-      { x: 10 + (width+10)*6, y: 10 + (height+10)*0 },
-
-      { x: 10 + (width+10)*0, y: 10 + (height+10)*1, type: 'cascade' },
-      { x: 10 + (width+10)*1, y: 10 + (height+10)*1, type: 'cascade' },
-      { x: 10 + (width+10)*2, y: 10 + (height+10)*1, type: 'cascade' },
-      { x: 10 + (width+10)*3, y: 10 + (height+10)*1, type: 'cascade' },
-      { x: 10 + (width+10)*4, y: 10 + (height+10)*1, type: 'cascade' },
-      { x: 10 + (width+10)*5, y: 10 + (height+10)*1, type: 'cascade' },
-      { x: 10 + (width+10)*6, y: 10 + (height+10)*1, type: 'cascade' },
-    ];
-    stacks.forEach(stack => stack.add = function(card){
-      if (card.parent) card.parent.child = null;
-      let target = this;
-      while (target.child) target = target.child;
-      card.x = target.x;
-      card.y = this.type === 'cascade' && target !== this ? target.y + 20 : target.y;
-      target.child = card;
-      card.parent = target;
-    });
-    const suites = 'Heart Diamond Spade Club'.split(' ');
-    const cards = new Array(52).fill()
-      .map((_, i) => ({
-        suite: suites[Math.floor(i / 13)],
-        value: i % 13 + 1,
-        x: 10, y: 10,
-        animated: true,
-      }))
-      .sort(_ => Math.random() - 0.5);
-    cards.forEach(stacks[0].add.bind(stacks[0]));
-    this.state = { stacks, cards };
-
-    async function moveCard(index, stack, reveal=false) {
-      this.setState(({ cards }) => {
-        const card = cards[index];
-        stack.add(card);
-        card.animated = true;
-        return { cards };
-      });
-      await sleep(0.3);
-      this.setState(({ cards }) => {
-        const card = cards[index];
-        card.revealed = reveal;
-        card.animated = false;
-        return { cards };
-      });
-    }
-    let cardIndex = 52 - 7 - 6 - 5 - 4 - 3 - 2 - 1;
-    for (let y=0; y<7; y++){
-      for (let x=y; x<7; x++){
-        await sleep(0.025);
-        moveCard.call(this, cardIndex++, stacks[x+6], x === y);
-      }
-    }
+    if (this.unsubscribe) this.unsubscribe();
+    this.unsubscribe = null;
+    this.game = new Solitare();
+    this.unsubscribe = this.game.subscribe(() => this.forceUpdate());
+    this.forceUpdate();
   }
 
-  handleReveal(cardIndex){
-    const { cards } = this.state;
-    cards[cardIndex].revealed = !cards[cardIndex].revealed;
-    this.setState({ cards });
-  }
-  handleMouseDown(cardIndex, event){
-    const { x, y } = event;
-    const { cards } = this.state;
-    let stackLength = 1;
-    let card = cards.splice(cardIndex, 1)[0];
-    card.animated = false;
-    cards.push(card);
-    while (card.child) {
-      stackLength++;
-      card = cards.splice(cards.indexOf(card.child), 1)[0];
-      card.animated = false;
-      cards.push(card);
-    }
-    this.grabbedCard = {
-      cardIndex: cards.length - stackLength,
-      last: { x, y },
-      from: { x: card.x, y: card.y }
-    };
-    this.setState({ cards })
+  handleMouseDown({ x, y, target: { tagName } }){
+    if (tagName !== 'use') return;
+    this.dragStart = { x, y };
+    this.setState({ holding: { dx: 0, dy: 0 } });
   }
   handleMouseMove(event){
-    if (!this.grabbedCard) return;
-    const { cardIndex, last } = this.grabbedCard;
-    const { cards, stacks } = this.state;
-    let card = cards[cardIndex];
-    card.x += event.x - last.x;
-    card.y += event.y - last.y;
-    this.grabbedCard.last = { x: event.x, y: event.y };
-    this.grabbedCard.target = [ ...cards, ...stacks ]
-      .filter(({ x: tX, y: tY }, index) =>
-        (index < cardIndex || index >= 52) &&
-        tX < card.x + width && tX > card.x - width &&
-        tY < card.y + height && tY > card.y - height
-      )
-      .sort((a, b) =>
-        Math.pow(Math.pow(card.x - a.x, 2) + Math.pow(card.y - a.y, 2), 0.5) -
-        Math.pow(Math.pow(card.x - b.x, 2) + Math.pow(card.y - b.y, 2), 0.5)
-      )[0];
-    while (card.child) {
-      card = card.child;
-      card.x += event.x - last.x;
-      card.y += event.y - last.y;
-    }
-    this.setState({ cards });
+    if (this.game.holdingStack.items.length === 0) return;
+    const { x, y } = event;
+    this.setState({ holding: {
+      dx: x - this.dragStart.x,
+      dy: y - this.dragStart.y
+    } });
   }
-  handleMouseUp(){
-    if (!this.grabbedCard) return;
-    const { cards } = this.state;
-    let card = cards[this.grabbedCard.cardIndex];
-    card.animated = true;
-    let stack = this.grabbedCard.target;
-    if (stack) {
-      while (stack.parent) stack = stack.parent;
-      stack.add(card);
-      while (card.child) {
-        card = card.child;
-        card.animated = true;
-        stack.add(card);
-      }
-    } else {
-      card.x = this.grabbedCard.from.x;
-      card.y = this.grabbedCard.from.y;
-    }
-    this.setState({ cards });
-    delete this.grabbedCard;
+  handleMouseUp(event){
+    this.game.holdingStack.reset();
   }
 
-  render({}, { cards, stacks }) {
-    return html `<div style="height: 100%; width: 100%;">
-      <div style="position: absolute; bottom: 0;">
+  render({}, { holding: { dx, dy } }) {
+    const scale = this.svgEl && this.svgEl.getScreenCTM().a;
+    const items = [];
+    function addItem(item){
+      const heald = item.stack === this.game.holdingStack;
+      if (heald) {
+        items.push(html`<${Item} key=${item.id} style=${{pointerEvents: 'none'}} x=${item.x + dx/scale} y=${item.y + dy/scale} image=${item.image} item=${item} />`);
+      } else {
+        items.push(html`<${Item} key=${item.id} x=${item.x} y=${item.y} image=${item.image} item=${item} />`);
+      }
+      if (item.items) item.items.forEach(addItem, this);
+    }
+    this.game.items.forEach(addItem, this);
+    return html `<div style=${{height: '100%', width: '100%'}}>
+      <div style=${{position: 'absolute', bottom: 0}}>
         <button onClick=${this.startSolitare}>Solitare</button>
-        <button onClick=${this.startShuffle}>Shuffle</button>
       </div>
       <svg
-        style="height: 100%; width: 100%; font-size: 12px;"
+        style=${{height: '100%', width: '100%', fontSize: 12}}
+        viewBox=${`0 0 ${this.game.width} ${this.game.height}`}
+        ref=${el => this.svgEl = el}
+        onMouseDown=${this.handleMouseDown}
         onMouseMove=${this.handleMouseMove}
         onMouseUp=${this.handleMouseUp}
       >
-        ${stacks.map(({ x, y }) =>
-          html`<rect ...${{ width, height, x, y }} rx="6" fill="transparent" stroke="black" stroke-width=".5"/>`
-        )}
-        ${cards.map((card, cardIndex) =>
-          html`<${Card} ...${card} index=${cardIndex} key=${`${card.suite} ${card.value}`}
-            onMouseDown=${this.handleMouseDown}
-            onReveal=${this.handleReveal}
-          />`
-        )}
+        ${items}
       </svg>
     </div>`;
   }
